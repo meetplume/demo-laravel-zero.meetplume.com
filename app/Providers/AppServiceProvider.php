e9545065324a5117a32ab405dbb418cbe01942f4
CHARLIE ETIENNE <ce@web-nancy.fr>
2026-03-16 23:54:08 +0100

    configure vault

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Plume\LaravelZeroDocsVault;
 use Illuminate\Support\ServiceProvider;
+use Meetplume\Plume\Facades\Plume;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Plume::configure()
+            ->vaults([
+                LaravelZeroDocsVault::class,
+            ]);
     }
 }
